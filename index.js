f368a1a8d73a6f1565b7b849fe8e2a896ffd408c
Mark Winterbottom <mark@winterbottom.me>
2016-09-11 16:09:30 +0100

    Updated.

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ var WebSocketServer = require("ws").Server;
 
 var wss = new WebSocketServer({port: 7007});
 
-
 console.log("Server is Running...");
-
 
 wss.broadcast = function broadcastMsg(msg) {
     wss.clients.forEach(function each(client) {
-        client.send(data);
+        client.send(msg);
     });
 };
 
 wss.on('connection', function connection(ws) {
     console.log("Connection Received.");
+    ws.on('message', function onMessage(msg) {
+        wss.broadcast(msg);
+    });
 });
